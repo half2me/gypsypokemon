06fc4092cc9d71219c967b875ad5c86d7781d9f5
Tamási Benjamin <halftome@yahoo.com>
2016-03-11 18:19:04 +0100

    added test Game()

--- src/main/java/iitema/gypsypokemon/Game.java
+++ src/main/java/iitema/gypsypokemon/Game.java
@@ -1,11 +1,47 @@
 package iitema.gypsypokemon;
 
+import iitema.gypsypokemon.elements.Direction;
+import iitema.gypsypokemon.elements.blocks.FieldInterface;
+import iitema.gypsypokemon.elements.blocks.Ground;
+import iitema.gypsypokemon.elements.blocks.Player;
+
 public class Game {
-    public void endGame(){
+
+    private FieldInterface field = new Ground();
+    private Player player = new Player(this, field);
+
+    public Game() {
+        FieldInterface f = this.field;
+        for (int i = 0; i < 10; i++) {
+            f.setNeighbor(Direction.RIGHT, new Ground());
+            f = f.getNeighbor(Direction.RIGHT);
+        }
+    }
+
+    public void startGame() {
+        this.player.step(Direction.RIGHT);
+        this.player.step(Direction.RIGHT);
+        this.player.step(Direction.RIGHT);
+        this.player.step(Direction.RIGHT);
+        this.player.step(Direction.RIGHT);
+        this.player.step(Direction.RIGHT);
+        this.player.step(Direction.RIGHT);
+        this.player.step(Direction.RIGHT);
+    }
+
+    public void endGame() {
+
+    }
+
+    public void pauseGame() {
 
     }
 
     public static void main(String[] args) {
         System.out.println("Gypsy Pokemon!");
+
+        Game game = new Game();
+
+        game.startGame();
     }
 }
