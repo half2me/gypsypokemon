c4e6f6c4f06b4cbd2cf4275aff61ed68ae3218e0
Benjamin Tamási <halftome@gmail.com>
2016-03-14 10:14:00 +0100

    Added docblocks

--- src/main/java/iitema/gypsypokemon/elements/Portal.java
+++ src/main/java/iitema/gypsypokemon/elements/Portal.java
@@ -14,14 +14,33 @@ public class Portal {
 
     public Color color;
 
+    /**
+     * Set one of the portals
+     *
+     * @param color color of the portal
+     * @param field field the portal is on
+     * @param side side it is valid on
+     */
     public static void set(Color color, FieldInterface field, Direction side) {
         Portal.portals.put(color, new Portal(field, side, color));
     }
 
+    /**
+     * Get a portal by its color
+     *
+     * @param color color of the portal
+     * @return portal
+     */
     public static Portal get(Color color) {
         return Portal.portals.get(color);
     }
 
+    /**
+     * Get a portal by the field it is on
+     *
+     * @param field field
+     * @return portal
+     */
     public static Portal get(FieldInterface field) {
         Portal p = null;
         for (Map.Entry<Color, Portal> e : Portal.portals.entrySet()) {
@@ -32,6 +51,13 @@ public class Portal {
         return p;
     }
 
+    /**
+     * Get a portal by the field and side of the field it is on
+     *
+     * @param field field the portal is one
+     * @param side side of the field the portal is on
+     * @return portal
+     */
     public static Portal get(FieldInterface field, Direction side) {
         Portal p = null;
         for (Map.Entry<Color, Portal> e : Portal.portals.entrySet()) {
@@ -42,17 +68,35 @@ public class Portal {
         return p;
     }
 
-    public Portal(FieldInterface field, Direction side, Color color) {
+    /**
+     * Portal constructor
+     *
+     * @param field field
+     * @param side side
+     * @param color color of the portal
+     */
+    private Portal(FieldInterface field, Direction side, Color color) {
         this.field = field;
         this.side = side;
         this.color = color;
     }
 
+    /**
+     * Field that the a portal links to
+     *
+     * This is the field that is pointed to by the opposite portal on the side it is on
+     * @return field
+     */
     public FieldInterface link() {
         Portal otherPortal = Portal.portals.get(this.color.getOpposite());
         return otherPortal.field.getNeighbor(otherPortal.side);
     }
 
+    /**
+     * Get the opposite portal
+     *
+     * @return opposite portal
+     */
     public Portal opposite() {
         return Portal.portals.get(this.color.getOpposite());
     }
